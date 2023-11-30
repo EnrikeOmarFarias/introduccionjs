cdf5c3d8ccc2dbdbf10c70856f000bec663dd8f7
enrikefarias <enrikefarias@gmail.com>
2023-11-30 17:04:25 -0300

    Return

--- js/08.js
+++ js/08.js
@@ -6,4 +6,20 @@ function sumar() {
 }
 
 
+let sumar2 = function() {
+  console.log(3 + 3)
+
+}
+
+sumar2()
+
+// Funciones con argumentos y parametros
+
+function sumar3(num1 =0, num2 =0) { // num1 y num2 son parametros
+  console.log(num1 + num2)
+}
+
+sumar3(10, 5) // Argumentos o valores
+sumar3(25, 50)
+sumar3()
 
