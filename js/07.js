ceb43cf40e4d2dbb52d32891cca029d80a9fac71
enrikefarias <enrikefarias@gmail.com>
2023-11-30 14:58:45 -0300

    For each

--- js/07.js
+++ js/07.js
@@ -2,7 +2,11 @@
 
 let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo"];
 
-let carrito = [{ nombre: "Notebook 13 pulgadas", precio: 500 }];
+let carrito = [
+  { nombre: "Notebook 13 pulgadas", precio: 500 },
+  { nombre: "Televisor", precio: 300 },
+  { nombre: "Monopatin", precio: 700 },
+];
 
 // ForEach
 
@@ -15,3 +19,13 @@ meses.forEach(function (mes) {
     console.log("Marzo si existe");
   }
 });
+
+// Includes
+let resultado = meses.includes("Marzo");
+console.log(resultado);
+
+// Reduce
+
+resultado = carrito.reduce(function (total, producto) {
+  return total + producto.precio;
+}, 0);
